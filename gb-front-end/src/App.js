3dba205b4139ee38f118a394061c54ac3b0db22a
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-24 22:10:38 +0200

    Adding editedPost function to pass the post data from Post to Edit components

--- gb-front-end/src/App.js
+++ gb-front-end/src/App.js
@@ -5,28 +5,32 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state={
-      route:'home'
-    }
+    this.state = {
+      route: 'home',
+      editedPost:null,
+    };
   }
 
   //Routing functions to handle renered components
-  Routing=(route)=>{
-    this.setState({route:route})
+  Routing = (route) => {
+    this.setState({ route: route });
+  };
+
+  //To save data from post component and pass it to the Edit component
+  editedPost=(post)=>{
+    this.setState({editedPost:post});
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.route==='home'?(
+        {this.state.route === 'home' ? (
           <div>
             <Post routeChange={this.Routing} />
           </div>
         ) : (
           <div></div>
-        )
-
-        }
+        )}
       </div>
     );
   }
