e61e514dcde5b6e47f1458bd5c17dc693e2a7bbe
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-24 16:14:07 +0200

    Importing post and user schema

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const Post = require('./Models/Posts')
-const User = require('./Models/Users')
+const Post = require('./Models/Posts');
+const User = require('./Models/Users');
 
 const app = express();
 
@@ -10,5 +10,12 @@ mongoose.connect(
   'mongodb+srv://user:test1945@cluster0-306uy.mongodb.net/test?retryWrites=true&w=majority',
   { useUnifiedTopology: true, useNewUrlParser: true }
 );
+
+app.get('/', (req, res) => {
+  Post.find()
+    .sort('-date')
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json('Unable to get data'));
+});
 
 app.listen(5000, () => console.log('App is working on port 5000'));
