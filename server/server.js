0968106eb79dadfae0ef5507c4c7e999507e178a
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-24 16:11:05 +0200

    Importing post and user schema

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const Post = require('./Models/Posts')
+const User = require('./Models/Users')
 
 const app = express();
 
