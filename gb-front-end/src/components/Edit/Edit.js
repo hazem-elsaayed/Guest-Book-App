7dd034c9cffe3a48e7b09960e510b25d0bff93e5
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-25 13:51:38 +0200

    Creating Edit form for post editing

--- gb-front-end/src/components/Edit/Edit.js
+++ gb-front-end/src/components/Edit/Edit.js
@@ -10,7 +10,24 @@ class Edit extends Component {
   }
 
   render() {
-    return <div></div>;
+    const { reouteChange, editedPost } = this.props;
+    return (
+      <div className="edit">
+        <fieldset>
+          <legend>Edit Your Post</legend>
+          <label>Title:</label>
+          <input type="text" defaultValue={editedPost.title}></input>
+          <br></br>
+          <br></br>
+          <label>Description:</label>
+          <textarea defaultValue={editedPost.description}></textarea>
+          <br></br>
+          <br></br>
+          <button>Submit</button>
+          <button>Cancel</button>
+        </fieldset>
+      </div>
+    );
   }
 }
 
