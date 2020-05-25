8baa302bcfa8d7f7935767b713e7f401a917c426
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-25 17:08:55 +0200

    Adding form to collect registering inputs

--- gb-front-end/src/components/Register/Register.js
+++ gb-front-end/src/components/Register/Register.js
@@ -1,19 +1,44 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
-class Register extends Component{
-  constructor(props){
-    super(props)
-    this.state={
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       name: '',
       email: '',
       password: '',
       password2: '',
-    }
+    };
   }
 
-  render(){
-    return <div></div>
+  render() {
+    return (
+      <div>
+        <fieldset>
+          <legend>Register</legend>
+          <label>Name:</label>
+          <input type="text"></input>
+          <br></br>
+          <br></br>
+          <label>Email:</label>
+          <input type="email"></input>
+          <br></br>
+          <br></br>
+          <label>password:</label>
+          <input type="password"></input>
+          <br></br>
+          <br></br>
+          <label>Re-type Password:</label>
+          <input type="password"></input>
+          <br></br>
+          <br></br>
+          <button> Register</button>
+          <br></br>
+          <button>Signin</button>
+        </fieldset>
+      </div>
+    );
   }
 }
 
-export default Register
+export default Register;
