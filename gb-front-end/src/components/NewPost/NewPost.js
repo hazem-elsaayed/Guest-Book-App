004a865e6df92fd829268368ce7a8fd82844f13b
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-25 15:10:58 +0200

    Creating form for new posts

--- gb-front-end/src/components/NewPost/NewPost.js
+++ gb-front-end/src/components/NewPost/NewPost.js
@@ -1,17 +1,29 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react';
 
 class NewPost extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-
+        <fieldset>
+          <legend>Add New Post</legend>
+          <label>Title:</label>
+          <input type="text"></input>
+          <br></br>
+          <br></br>
+          <label>Description:</label>
+          <textarea></textarea>
+          <br></br>
+          <br></br>
+          <button>Submit</button>
+          <button>Cancel</button>
+        </fieldset>
       </div>
-    )
+    );
   }
 }
 
-export default NewPost
+export default NewPost;
