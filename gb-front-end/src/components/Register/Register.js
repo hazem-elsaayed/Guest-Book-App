7b02771ee2abd8233da785438dd1bd1e93798313
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-25 17:20:34 +0200

    Updating function with fetch request to store the new user in the database

--- gb-front-end/src/components/Register/Register.js
+++ gb-front-end/src/components/Register/Register.js
@@ -31,12 +31,30 @@ class Register extends Component {
     this.setState({ password2: txt.target.value });
   };
 
-  submit =()=>{
-    if (this.state.password !== this.state.password2){
-      return window.alert('Re-Typed Password is Not Matching')
+  submit = () => {
+    if (this.state.password !== this.state.password2) {
+      return window.alert('Re-Typed Password is Not Matching');
     }
-    
-  }
+    fetch(`http://localhost:3000/register`, {
+      method: 'post',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email.toLowerCase(),
+        password: this.state.password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.id) {
+          this.props.loadUser(data);
+          this.props.routeChange('home');
+        } else if (data === 'Worng Inputs') {
+          window.alert('Please Fill All The Fields With Proper Credintals ');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
 
   render() {
     return (
