e50e76a07d63b6d456126b792867ef097ee8846e
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-25 17:04:25 +0200

    Adding state to the store the name, email and password

--- gb-front-end/src/components/Register/Register.js
+++ gb-front-end/src/components/Register/Register.js
@@ -3,6 +3,12 @@ import React, {Component} from 'react'
 class Register extends Component{
   constructor(props){
     super(props)
+    this.state={
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+    }
   }
 
   render(){
