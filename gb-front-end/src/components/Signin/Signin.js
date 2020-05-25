851202680cf6a679c1a5707a861f5e6bd39ecfb6
Zoma <hazem_ahmed_93@yahoo.com>
2020-05-25 15:59:44 +0200

    Creating form to handle email and password

--- gb-front-end/src/components/Signin/Signin.js
+++ gb-front-end/src/components/Signin/Signin.js
@@ -10,7 +10,27 @@ class Signin extends Component {
   }
 
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Welcome to the Geust Book App</h1>
+        <h4>Please Sign in or Register to get to the App</h4>
+        <fieldset>
+          <legend>Sign In</legend>
+          <label>Email:</label>
+          <input type="text" autoComplete="on"></input>
+          <br></br>
+          <br></br>
+          <label>Password:</label>
+          <input type="password"></input>
+          <br></br>
+          <br></br>
+          <button>Submit</button>
+          <br></br>
+          <br></br>
+          <button>Register</button>
+        </fieldset>
+      </div>
+    );
   }
 }
 
